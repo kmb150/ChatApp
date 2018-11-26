342d21a5554f70d978074dd8737567327052a7e5
Kristian Babulev <kriso4os@abv.bg>
2018-11-26 11:59:57 +0200

    added message model and database

---
+++ ChatApp/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+namespace ChatApp.Models
+{
+    public class Message
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public int FromUser { get; set; }
+        public int ToUser { get; set; }
+    }
+
+}
