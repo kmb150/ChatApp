f1c74b2c271e41d51fa823efd0917333d6ea5351
Kristian Babulev <kriso4os@abv.bg>
2018-11-26 12:13:41 +0200

    added the contacts model and database

---
+++ ChatApp/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ChatApp.Models
+{
+    public class Contact
+    {
+        [Key, Column(Order = 0)]
+        public int UserId { get; set; }
+        [Key, Column(Order = 1)]
+        public int ContactId { get; set; }
+    }
+    public class ContactsContext: DbContext
+    {
+        public System.Data.Entity.DbSet<ChatApp.Models.Contact> Contacts { get; set; }
+    }
+}
